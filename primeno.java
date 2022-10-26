320cd899a45c42e435bb53311b64e7b21d63318c
hellman0-0 <77567765+hellman0-0@users.noreply.github.com>
2022-10-27 03:13:50 +0530

    update program using function

--- primeno.java
+++ primeno.java
@@ -3,16 +3,20 @@ public class primeno
  /**
      * @param args
      */
-    public static void main(String args[]){
-        int i=2;
-        System.out.println("Prime Numbers Are:");
-      for(i=2;i<=10;i++){
-        int count,j;
-        count=0;
+    static int prime(int i){
+        int j;
+        int count=0;
         for(j=2;j<=i/2;j++){
             if(i%j==0){
                 count++;
             }}
+        return count;
+    }
+     public static void main(String args[]){
+        int i=2,count;
+        System.out.println("Prime Numbers Are:");
+      for(i=2;i<=10;i++){
+        count=prime(i);
         if(count==0){
             System.out.println(i);
         }}}}
